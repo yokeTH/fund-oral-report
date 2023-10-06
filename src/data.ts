98cfdc259036e201a058122ac2fc0d5b249a9a3a
yokeTH <sern.dev@gmail.com>
2023-10-06 19:52:26 +0700

    base web without impliment anything

---
+++ src/data.ts
@@ -0,0 +1,205 @@
+import { Response } from "./interfaces/response";
+
+const data: Response = {
+  success: true,
+  data: {
+    id: "64ef405805e44be859f021ef",
+    studentID: "6530162621",
+    name: "ธนพนธ์",
+    lastname: "เจาะดี",
+    sections: [
+      {
+        sectionName: "2023-1",
+        sectionVideoData: [
+          {
+            videoTitle: "N/A",
+            percentage: 47,
+          },
+          {
+            videoTitle: "งานย้อมสีฟัน",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 1.1 Dental Vocab",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 11.1 การทำฟันเด็ก",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 4.1 น้ำลาย Saliva",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 3.1 การพัฒนาการของฟันเเละความผิดปกติ",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 8.4 นอนกัดฟัน",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 11.3 ฟันเก (ไม่มีเอกสาร คลิปสั้นๆ)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 7.2 อาหารเเละฟัน (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 5.2 Dental Caries  (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 9.1 ผลิตภัณฑ์ทางทันตกรรม",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 3.4 จัดฟันใสมีกี่แบบ",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 3.2 กลิ่นปาก  (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 12.3 ครอบฟัน (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 12.1 การทำฟันในผู้สูงอายุ",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 8.3 เหล็กจัดฟันทิ่มเเก้ม ทำไงดี",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 2.2 การทำฟันของนิสิตจุฬา  (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 9.3 นำ้ยาบ้วนปาก",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 9.2 การเเปรงฟันเเละใช้ไหมขัดฟัน (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 3.3 เเปรงฟันเด็กเล็ก (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle:
+              "Lecture 4.2 น้ำตาลส่งผลกระทบต่อสมองอย่างไร  (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Orientation 6 Aug 2023",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 6.3 เมื่อฟันแท้หลุด ควรทำอย่างไร",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 6.1 ฟลูออไรด์",
+            percentage: 100,
+          },
+          {
+            videoTitle: "lecture 9.4 แผลในปาก วิธีการสังเกตและรักษา",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 7.3 ถอนฟันนำ้นมเองได้มั้ย",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 10.3 Fluoride อันตรายมั้ย",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 8.1 การดูเเลช่องปากด้วยตนเอง",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 10.1 งานต่างๆทางทันตกรรม",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 6.2 การจัดฟันใส Invisalign (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 12.2 ฟันคุด (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle:
+              "Lecture 7.1 อาหารเพื่อสุขภาพฟันเเละร่างกายที่ดี Food for health",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 1.2 การทำฟันช่วงโควิด (ไม่มีเอกสาร)",
+            percentage: 100,
+          },
+          {
+            videoTitle:
+              "Lecture 2.3 การจัดฟันร่วมกับการผ่าตัดขากรรไกร เผื่อใครต้องรักษาเเบบนี้ค่ะ",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 10.2 Digital dentistry  (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 1.3 เเนะนำคณะทันตเเพทยศาสตร์ จุฬา",
+            percentage: 100,
+          },
+          {
+            videoTitle:
+              "Lecture 8.2 ช่องปากบอกสุขภาพร่างกายอย่างไร (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle:
+              "Lecture 2.1 Oral-dental anatomy โครงสร้างช่องปากเเละฟัน",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 5.3 Cheese ป้องกันฟันผุได้จริงมั้ย",
+            percentage: 80,
+          },
+          {
+            videoTitle:
+              "Lecture 4.3 Dental Stem Cells เซลล์ต้นกำเนิดจากฟันเเละช่องปาก",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 11.4 นำ้ยาบ้วนปาก ฟันดำ",
+            percentage: 0,
+          },
+          {
+            videoTitle: "zoom สรุปงาน",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 5.1 ฟันผุเเละโรคปริทันต์",
+            percentage: 100,
+          },
+          {
+            videoTitle: "Lecture 11.2 การเคลื่อนฟันระหว่างจัดฟัน (ไม่มีเอกสาร)",
+            percentage: 0,
+          },
+          {
+            videoTitle: "Lecture 10.4 การอุดฟัน",
+            percentage: 0,
+          },
+        ],
+      },
+    ],
+    updateAt: new Date("2023-10-02T09:40:50.509Z"),
+  },
+};
