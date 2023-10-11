84dc955f3ee2972a5fb040123ecfb15d41607200
yokeTH <sern.dev@gmail.com>
2023-10-11 21:07:20 +0700

    refactor/Extract components

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import axios from "axios";
 import FormData from "form-data";
+import UploadSuccess from "@/components/aleart/uploadSuccess";
+import UploadError from "@/components/aleart/uploadError";
 
 export default function Upload() {
   const [file, setFile] = useState<File>();
@@ -46,42 +48,12 @@ export default function Upload() {
         <div className="card-body">
           <h2 className="card-title text-base-content">Upload ไฟล์ Excel</h2>
           {status ? (
-            <div className="alert alert-success">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Success!</span>
-            </div>
+            <UploadSuccess/>
           ) : (
             ""
           )}
           {error ? (
-            <div className="alert alert-error">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Error!</span>
-            </div>
+            <UploadError/>
           ) : (
             ""
           )}
